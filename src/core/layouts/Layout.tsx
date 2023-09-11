3623172ecb64dd4aee5a7cf084437e872a426fa5
IvanOrsh <ksironov@mail.ru>
2023-09-11 21:53:38 +0300

    test: scaffold with mantine app shell

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
-import React, { FC, PropsWithChildren, Suspense } from "react"
+import React, { PropsWithChildren, Suspense } from "react"
 import { BlitzLayout } from "@blitzjs/next"
+import { AppShell, Navbar, Header, Text, Footer } from "@mantine/core"
+import { Horizontal, Vertical } from "mantine-layout-components"
 
 type LayoutProps = {
   title?: string
@@ -12,6 +14,8 @@ const Layout: BlitzLayout<PropsWithChildren<LayoutProps>> = ({
   children,
   maxWidth = 800,
 }) => {
+  const thisYear = new Date().getFullYear()
+
   return (
     <>
       <Head>
@@ -19,16 +23,50 @@ const Layout: BlitzLayout<PropsWithChildren<LayoutProps>> = ({
         <link rel="icon" href="/image/favicon.ico" />
       </Head>
 
-      <Suspense fallback={"Loading..."}>
-        <div
-          style={{
-            width: "100%",
-            maxWidth,
-          }}
-        >
-          {children}
-        </div>
-      </Suspense>
+      <AppShell
+        padding="md"
+        // navbar={
+        //   <Navbar width={{ base: 300 }} height={500} p="xs">
+        //     {/* Navbar content */}
+        //   </Navbar>
+        // }
+        header={
+          <Header height={45} p="xs">
+            <Horizontal debug fullH>
+              <Text fw="bold">Refapp</Text>
+            </Horizontal>
+          </Header>
+        }
+        footer={
+          <Footer height={35}>
+            <Horizontal fullH fullW center>
+              <Text fz="xs" color="dimmed">
+                address, copyright, {thisYear}
+              </Text>
+            </Horizontal>
+          </Footer>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        })}
+      >
+        {/* Your application here */}
+        <Suspense fallback={"Loading..."}>
+          <div
+            style={{
+              width: "100%",
+              maxWidth,
+            }}
+          >
+            <Vertical debug fullW fullH>
+              {children}
+            </Vertical>
+          </div>
+        </Suspense>
+      </AppShell>
     </>
   )
 }
