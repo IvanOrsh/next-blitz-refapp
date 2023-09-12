87f1f9be825a451d8b9794be09498d5f7fccb31d
IvanOrsh <ksironov@mail.ru>
2023-09-12 03:03:06 +0300

    feat: main authenticaion on home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,21 @@
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import { UserInfo } from "@/core/components/UserInfo"
+import { AuthenticationForm } from "@/core/components/MainAuthenticationForm"
+import { useCurrentUser } from "@/features/users/hooks/useCurrentUser"
+import { Vertical } from "mantine-layout-components"
 
 const Home: BlitzPage = () => {
+  const currentUser = useCurrentUser()
+
   return (
     <Layout title="Home">
-      {/* Auth */}
-
-      <div>
-        <UserInfo />
-      </div>
+      {currentUser && <UserInfo />}
+      {!currentUser && (
+        <Vertical fullH fullW center>
+          <AuthenticationForm />
+        </Vertical>
+      )}
     </Layout>
   )
 }
